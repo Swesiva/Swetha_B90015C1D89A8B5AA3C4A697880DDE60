88cb9ab934c261608bae5d4b79a6ce4e7b11465a
Swesiva <144222545+Swesiva@users.noreply.github.com>
2023-09-15 13:15:43 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,10 @@
+def factorial_iterative(n):
+    #base case:factorial of 0 is 1
+    if n==0:
+       return 1
+    else:
+       factorial=1
+       for i in range(1,n+1):
+           factorial*=i
+       return factorial
+print(factorial_iterative(5))
